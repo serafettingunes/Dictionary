8e6692e58fa9a8b6628a58185e4f5868844b30ca
serafettingunes <54938342+attila40x@users.noreply.github.com>
2021-01-23 18:49:29 +0300

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string, string> myDictionary = new Dictionary.MyDictionary<int, string,string>();
+            myDictionary.Ekle(1, "Şerafettin","Güneş");
+            
+        }
+    }
+    class MyDictionary <T1,T2,T3>
+    {
+        public void Ekle(T1 item,T2 item2, T3 item3)
+        {
+            Console.WriteLine(item+" "+item2+" "+item3);
+        }
+    }
+}
